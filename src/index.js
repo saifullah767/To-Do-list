bf0fa9838f4fd2daaa521de33764a4014ce5f888
skj99667788@gmail.com <skj99667788@gmail.com>
2022-04-27 05:48:23 +0500

    eslint error

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import "./style.css";
+import './style.css';
 
 const lists = [
   {
-    description: "Descriptions of task 1.",
+    description: 'Descriptions of task 1.',
     completed: false,
     index: 1,
   },
   {
-    description: "Descriptions of task 2.",
+    description: 'Descriptions of task 2.',
     completed: false,
     index: 2,
   },
   {
-    description: "Descriptions of task 3.",
+    description: 'Descriptions of task 3.',
     completed: false,
     index: 3,
   },
   {
-    description: "Descriptions of task 4.",
+    description: 'Descriptions of task 4.',
     completed: false,
     index: 4,
   },
   {
-    description: "Descriptions of task 5.",
+    description: 'Descriptions of task 5.',
     completed: false,
     index: 5,
   },
 ];
 
-const section = document.getElementById("wholeList");
+const section = document.getElementById('wholeList');
 window.onload = () => {
   lists.forEach((toDo) => {
     const listHTML = `<li>
@@ -36,6 +36,6 @@ window.onload = () => {
     <label class='label' tabindex='0'>${toDo.description}</label>
     <i class='fa-solid fa-ellipsis-vertical'></i>
 </li>`;
-    section.insertAdjacentHTML("beforeend", listHTML);
+    section.insertAdjacentHTML('beforeend', listHTML);
   });
 };
